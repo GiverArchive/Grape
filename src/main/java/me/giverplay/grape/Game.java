9b3d357f7059eb0d9010904b5b62dfef618e2a47
GiverPlay007 <play.giver.007@gmail.com>
2021-11-06 23:28:42 -0300

    Created window

--- src/main/java/me/giverplay/grape/Game.java
+++ src/main/java/me/giverplay/grape/Game.java
@@ -1,11 +1,34 @@
 package me.giverplay.grape;
 
+import javax.swing.JFrame;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
 public class Game {
 
+  public static final int SCREEN_WIDTH = 720;
+  public static final int SCREEN_HEIGHT = 460;
+
+  private final Canvas canvas;
   private final Loop loop;
 
   public Game() {
-    this.loop = new Loop(60, this::tick);
+    loop = new Loop(60, this::tick);
+    canvas = new Canvas();
+    canvas.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+
+    JFrame frame = new JFrame("Grape");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setResizable(false);
+    frame.add(canvas);
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+
+    canvas.createBufferStrategy(3);
+
+    frame.setVisible(true);
   }
 
   public void start() {
@@ -21,7 +44,13 @@ public class Game {
   }
 
   public void draw() {
+    Graphics graphics = canvas.getBufferStrategy().getDrawGraphics();
 
+    graphics.setColor(new Color(0xB1B1B1));
+    graphics.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    graphics.dispose();
+
+    canvas.getBufferStrategy().show();
   }
 
   private void tick() {
